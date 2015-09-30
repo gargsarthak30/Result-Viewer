adb2a961ac70da22e6dda9cab780c678bded4b31
Sarthak Garg <gargsarthak30@gmail.com>
2015-09-30 23:07:42 +0530

    updates

---
+++ result.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 30, 2015 at 06:59 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `result`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `master`
+--
+
+CREATE TABLE IF NOT EXISTS `master` (
+  `College_Id` varchar(20) NOT NULL,
+  `College_Name` varchar(50) NOT NULL,
+  PRIMARY KEY (`College_Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `master`
+--
+
+INSERT INTO `master` (`College_Id`, `College_Name`) VALUES
+('sobsc', 'SCHOOL OF BUDDHIST STUDIES AND CIVILIZATION'),
+('sobt', 'SCHOOL OF BIOTECHNOLOGY'),
+('soe', 'SCHOOL OF ENGINEERING'),
+('sohss', 'SCHOOL OF HUMANITIES AND SOCIAL SCIENCES'),
+('soict', 'SCHOOL OF INFORMATION COMMUNICATION TECHNOLOGY'),
+('soljg', 'SCHOOL OF LAW, JUSTICE AND GOVERNANCE'),
+('som', 'SCHOOL OF MANAGEMENT'),
+('sovsas', 'SCHOOL OF VOCATIONAL STUDIES AND APPLIED SCIENCES');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sobsc`
+--
+
+CREATE TABLE IF NOT EXISTS `sobsc` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sobt`
+--
+
+CREATE TABLE IF NOT EXISTS `sobt` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soe`
+--
+
+CREATE TABLE IF NOT EXISTS `soe` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sohss`
+--
+
+CREATE TABLE IF NOT EXISTS `sohss` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soict`
+--
+
+CREATE TABLE IF NOT EXISTS `soict` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `soict`
+--
+
+INSERT INTO `soict` (`RegistrationNumber`, `Score`, `Semester`) VALUES
+('13ics047', '9.31', '4'),
+('13ics047', '7.77', '3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `soljg`
+--
+
+CREATE TABLE IF NOT EXISTS `soljg` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `som`
+--
+
+CREATE TABLE IF NOT EXISTS `som` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sovsas`
+--
+
+CREATE TABLE IF NOT EXISTS `sovsas` (
+  `RegistrationNumber` varchar(12) NOT NULL,
+  `Score` varchar(5) NOT NULL,
+  `Semester` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `User_Id` int(5) NOT NULL AUTO_INCREMENT,
+  `College_Id` varchar(20) NOT NULL,
+  `RegistrationNumber` varchar(20) NOT NULL,
+  `Password` varchar(60) NOT NULL,
+  PRIMARY KEY (`User_Id`),
+  KEY `College_Id` (`College_Id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=70 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`User_Id`, `College_Id`, `RegistrationNumber`, `Password`) VALUES
+(69, 'soict', '13ics047', '$2y$10$/wYD1GrI4/FqM4oDsZTwQugaam5mCRQd2u.hKP7z9IFCmzUOy7zBa');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`College_Id`) REFERENCES `master` (`College_Id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
