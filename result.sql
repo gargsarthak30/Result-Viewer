c2e01887ad746f77c313af45b26da1473a2cb2b6
Sarthak Garg <gargsarthak30@gmail.com>
2015-10-07 22:30:45 +0530

    new database uploaded

--- result.sql
+++ result.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 30, 2015 at 06:59 PM
+-- Generation Time: Oct 07, 2015 at 06:59 PM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -19,6 +19,19 @@ SET time_zone = "+00:00";
 --
 -- Database: `result`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `Name` varchar(50) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(60) NOT NULL,
+  `Email` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,7 +68,8 @@ INSERT INTO `master` (`College_Id`, `College_Name`) VALUES
 CREATE TABLE IF NOT EXISTS `sobsc` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -67,7 +81,8 @@ CREATE TABLE IF NOT EXISTS `sobsc` (
 CREATE TABLE IF NOT EXISTS `sobt` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -79,7 +94,8 @@ CREATE TABLE IF NOT EXISTS `sobt` (
 CREATE TABLE IF NOT EXISTS `soe` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -91,7 +107,8 @@ CREATE TABLE IF NOT EXISTS `soe` (
 CREATE TABLE IF NOT EXISTS `sohss` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -103,16 +120,17 @@ CREATE TABLE IF NOT EXISTS `sohss` (
 CREATE TABLE IF NOT EXISTS `soict` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `soict`
 --
 
-INSERT INTO `soict` (`RegistrationNumber`, `Score`, `Semester`) VALUES
-('13ics047', '9.31', '4'),
-('13ics047', '7.77', '3');
+INSERT INTO `soict` (`RegistrationNumber`, `Score`, `Semester`, `Subject`) VALUES
+('13ics047', '75', '3', 'MA-103'),
+('13ics047', '90', '4', 'EC-104');
 
 -- --------------------------------------------------------
 
@@ -123,7 +141,8 @@ INSERT INTO `soict` (`RegistrationNumber`, `Score`, `Semester`) VALUES
 CREATE TABLE IF NOT EXISTS `soljg` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -135,7 +154,8 @@ CREATE TABLE IF NOT EXISTS `soljg` (
 CREATE TABLE IF NOT EXISTS `som` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -147,7 +167,8 @@ CREATE TABLE IF NOT EXISTS `som` (
 CREATE TABLE IF NOT EXISTS `sovsas` (
   `RegistrationNumber` varchar(12) NOT NULL,
   `Score` varchar(5) NOT NULL,
-  `Semester` varchar(5) NOT NULL
+  `Semester` varchar(5) NOT NULL,
+  `Subject` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -163,14 +184,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Password` varchar(60) NOT NULL,
   PRIMARY KEY (`User_Id`),
   KEY `College_Id` (`College_Id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=70 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=76 ;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`User_Id`, `College_Id`, `RegistrationNumber`, `Password`) VALUES
-(69, 'soict', '13ics047', '$2y$10$/wYD1GrI4/FqM4oDsZTwQugaam5mCRQd2u.hKP7z9IFCmzUOy7zBa');
+(69, 'soict', '13ics047', '$2y$10$/wYD1GrI4/FqM4oDsZTwQugaam5mCRQd2u.hKP7z9IFCmzUOy7zBa'),
+(72, 'soict', '13ics022', '$2y$10$ggHqI/IT4a8/frO8wGnWyufA7KX/tSKfAaKi.k5KMI0ti8z51H2lW'),
+(75, 'soict', '13ics015', '$2y$10$rRdBy/6EdciQzCjRXwBiKOUtj9AULZdUmZ1EQI.OvBsRz05Ob0.xO');
 
 --
 -- Constraints for dumped tables
