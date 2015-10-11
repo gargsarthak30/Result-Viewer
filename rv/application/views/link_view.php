37094f01f5663c4a1c71e9cf1b526cfbbf088258
Rajat Saxena <rajat.saxena035@gmail.com>
2015-10-11 18:45:24 +0530

    Better UI
    I will do the remaining work tomorrow

--- rv/application/views/link_view.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-	<title>Result-Viewer</title>
-	<script src="<?php echo base_url('assets/jquery-2.1.4.min.js') ?>"></script>
-    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
-	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js') ?>"></script>
-</head>
-
-<body>
---
+++ web/application/views/link_view.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Result-Viewer</title>
+	<script src="<?php echo base_url('assets/jquery-2.1.4.min.js') ?>"></script>
+    <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
+	<link href='https://fonts.googleapis.com/css?family=Sigmar+One' rel='stylesheet' type='text/css'>
+	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.js') ?>"></script>
+	<style>
+		body
+		{
+  background: -webkit-linear-gradient(90deg, #8e9eab 10%, #eef2f3 90%); /* Chrome 10+, Saf5.1+ */
+  background:    -moz-linear-gradient(90deg, #8e9eab 10%, #eef2f3 90%); /* FF3.6+ */
+  background:     -ms-linear-gradient(90deg, #8e9eab 10%, #eef2f3 90%); /* IE10 */
+  background:      -o-linear-gradient(90deg, #8e9eab 10%, #eef2f3 90%); /* Opera 11.10+ */
+  background:         linear-gradient(90deg, #8e9eab 10%, #eef2f3 90%); /* W3C */
+        
+                
+		}
+	</style>
+	</head>
+
+<body>
