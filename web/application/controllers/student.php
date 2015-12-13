ff26d97e2492253f404ad632ef47c3f04e840c32
Rajat Saxena <rajat.saxena035@gmail.com>
2015-12-13 19:47:43 +0530

    some updates

--- web/application/controllers/student.php
+++ web/application/controllers/student.php
@@ -2,18 +2,26 @@
 
 class Student extends CI_Controller {
 	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('result_model');
+	}
+
 	public function details()
 	{
 			$this->load->library('form_validation');
 			$this->load->view('theme/common/link');
 			$this->load->view('theme/homepage/header');
-			$this->load->view('theme/student/signin');
+			$this->load->view('theme/student/details');
 			$this->load->view('theme/common/footer');
 	}
 	
 	public function result()
 	{
-			$data['reg_no'] = $this->input->post('reg_no');
+			$reg_no = $this->input->post('reg_no');
+			$data['stu_res'] = $this->result_model->student_result($reg_no);
+			$data['reg_no'] = $reg_no;
 			$this->load->view('theme/common/link');
 			$this->load->view('theme/student/header');
 			$this->load->view('theme/student/result',$data);
